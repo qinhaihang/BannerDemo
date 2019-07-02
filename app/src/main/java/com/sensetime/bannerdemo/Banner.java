c49ae5c5382c99bf26e52cbd318e9046a91555bf
qinhaihang_vendor <qinhaihang_vendor@sensetime.com>
2019-07-02 17:11:44 +0800

    Initial commit

---
+++ app/src/main/java/com/sensetime/bannerdemo/Banner.java
@@ -0,0 +1,81 @@
+package com.sensetime.bannerdemo;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+/**
+ * @author qinhaihang_vendor
+ * @version $Rev$
+ * @time 2019/7/2 16:19
+ * @des
+ * @packgename com.sensetime.bannerdemo
+ * @updateAuthor $Author$
+ * @updateDate $Date$
+ * @updateDes
+ */
+public class Banner extends ViewPager {
+
+    private BannerCallback mBannerCallback;
+
+    private Runnable player = new Runnable() {
+        @Override
+        public void run() {
+            play();
+        }
+    };
+
+    public Banner(@NonNull Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public Banner(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+
+    }
+
+    public void start(long showTime) {
+        stop();
+        postDelayed(player, showTime);
+    }
+
+    private synchronized void play() {
+        PagerAdapter pagerAdapter = getAdapter();
+        if (pagerAdapter != null) {
+            int count = pagerAdapter.getCount();
+
+            int currentItem = getCurrentItem();
+
+            if (currentItem < count - 1) {
+                currentItem++;
+                setCurrentItem(currentItem);
+            } else {
+                //回调
+                currentItem = 0;
+                if(mBannerCallback != null) mBannerCallback.playEnd();
+            }
+
+        }
+    }
+
+    public void stop() {
+        removeCallbacks(player);
+    }
+
+    public void setBannerCallback(BannerCallback bannerCallback) {
+        mBannerCallback = bannerCallback;
+    }
+
+    interface BannerCallback {
+        void playEnd();
+    }
+
+}
---
+++ app/src/main/java/com/sensetime/bannerdemo/MainActivity.java
@@ -0,0 +1,83 @@
+package com.sensetime.bannerdemo;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.view.PagerAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Banner vp_banner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        vp_banner = findViewById(R.id.vp_banner);
+
+        ArrayList<Integer> images = new ArrayList<>();
+        images.add(R.drawable.image1);
+        images.add(R.drawable.image2);
+        images.add(R.drawable.image3);
+
+        MyPageAdapter myPageAdapter = new MyPageAdapter(this,images);
+        vp_banner.setAdapter(myPageAdapter);
+        vp_banner.setBannerCallback(new Banner.BannerCallback() {
+            @Override
+            public void playEnd() {
+                Toast.makeText(MainActivity.this,"已经到最后",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public void click(View view) {
+        vp_banner.start(1000);
+    }
+
+    class MyPageAdapter extends PagerAdapter{
+
+        ArrayList<Integer> images;
+        private Context mContext;
+
+        public MyPageAdapter(Context context,ArrayList<Integer> images) {
+            this.images = images;
+            mContext = context;
+        }
+
+        @Override
+        public int getCount() {
+            return images.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+            return view == o;
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+            ImageView imageView = new ImageView(mContext);
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), images.get(position));
+            imageView.setImageBitmap(bitmap);
+            container.addView(imageView);
+            return imageView;
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            container.removeView((ImageView)object);
+        }
+    }
+}
